7a52ad323e8d5a5ad5bd49e5cc4c953c9130adb9
Enri44 <enry.gerardi@gmail.com>
2023-03-31 14:55:01 +0200

    Aggiunta classi Corpo e Posizione

---
+++ src/Posizione.java
@@ -0,0 +1,35 @@
+public class Posizione {
+
+    private double x;
+    private double y;
+
+    public Posizione(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double get_distanza(Posizione p2){
+        return Math.sqrt(Math.pow(x-p2.getX(), 2) + Math.pow(y-p2.getY(), 2));
+    }
+
+    public Posizione posizione_relativa(Posizione riferimento){
+        return new Posizione(x-riferimento.getX(), y-riferimento.getY());
+    }
+
+}
