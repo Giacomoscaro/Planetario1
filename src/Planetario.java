46340874045a6f297b94bddf2b2c8c96ef905f31
Giacomoscaro <giacomoscaroni2003@gmail.com>
2023-03-31 21:04:03 +0200

    sistemato gli input del metodo creaStella() in Planetario, inoltre rimosso lo stesso metodo dal costruttore planetario, perchè te ne faceva creare 2 ogni volta

--- src/Planetario.java
+++ src/Planetario.java
@@ -1,20 +1,21 @@
+import it.kibo.fp.lib.InputData;
 import it.unibs.fp.mylib.InputDati;
 
 import java.util.ArrayList;
 
 public class Planetario {
-    private ArrayList<Corpo> lista_corpi;
+    private ArrayList<Corpo> lista_corpi = new ArrayList<Corpo>(10);;
     private String nome_sistema;
     public Planetario(){
         System.out.println("Crea nuovo it.unibs.fp.mylib.Planetario");
-        creaStella();
+
     }
     public void creaStella(){
-        String nome = InputDati.leggiStringaNonVuota("nome:");
-        Double massa = InputDati.leggiDouble("massa:");
-        Double raggio_corpo = InputDati.leggiDouble("raggio corpo: ");
-        Stella stella = new Stella(nome , massa, raggio_corpo);
-	lista_corpi = new ArrayList<Corpo>();
+        String nome = InputData.readNonEmptyString("nome:",true);
+        Double massa = InputData.readDouble("massa:");
+        Double raggio_corpo = InputData.readDouble("raggio corpo: ");
+        Stella stella = new Stella(nome,massa,raggio_corpo);
+	    //lista_corpi = new ArrayList<Corpo>();
         lista_corpi.add(stella);
     }
     public String getNome_sistema() {
