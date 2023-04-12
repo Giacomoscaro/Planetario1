48ba99557222d982387eb3977ecc5aeed68ffb71
Enri44 <enry.gerardi@gmail.com>
2023-04-12 16:22:13 +0200

    Miglioramento menu e cambio formattazione CDM

--- src/MenuPrincipale.java
+++ src/MenuPrincipale.java
@@ -3,7 +3,7 @@ import it.kibo.fp.lib.InputData;
 import it.kibo.fp.lib.Menu;
 
 public class MenuPrincipale {
-    private final String[] scelte = {"Aggiungi un pianeta", "Aggiungi una luna", "Visualizza tutti i corpi", "Resetta il sistema", "Rimuovi un pianeta", "Rimuovi una luna", "Visualizza un corpo a scelta", "Controlla se un corpo appartiene al sistema", "Visualizza tutti i satelliti di un corpo", "Calcola il centro di massa del sistema", "Calcola la rotta migliore tra due corpi", "Controllo collisioni"};
+    private final String[] scelte = {"Menu Editor Corpi", "Menu Info Corpi", "Menu Funzionalità"};
     private final String[] editor = {"Aggiungi un Pianeta", "Aggiungi una Luna", "Rimuovi un Pianeta", "Rimuovi una Luna"};
     private final String[] info = {"Visualizza tutti i Corpi", "Visualizza un Corpo a scelta", "Visualizza i Satelliti di un corpo", "Controllo che un corpo appartiente al Sistema"};
     private final String[] funzionalita = {"Calcola il centro di massa del Sistema", "Controlla le Collisioni", "Calcola le rotta migliore tra due corpi", "Resetta il Sistema"};
@@ -17,26 +17,34 @@ public class MenuPrincipale {
     }
     public void editor(Planetario p1) {
         int scelta = m1.choose();
-        switch (scelta) {
-            case 1 -> p1.creaPianeta();
-            case 2 -> p1.creaLuna();
-            case 3 -> {
-                p1.stampa_pianeti();
-                String nome_r = InputData.readNonEmptyString("Inserisci il nome del pianeta da rimuovere: ", true);
-                Pianeta pianeta_r = (Pianeta) p1.toCorpo(nome_r);
-                p1.rimuovi_pianeta(pianeta_r);
+        boolean principale;
+            switch (scelta) {
+                case 1 -> p1.creaPianeta();
+                case 2 -> p1.creaLuna();
+                case 3 -> {
+                    p1.stampa_pianeti();
+                    String nome_r = InputData.readNonEmptyString("Inserisci il nome del pianeta da rimuovere: ", true);
+                    Pianeta pianeta_r = (Pianeta) p1.toCorpo(nome_r);
+                    p1.rimuovi_pianeta(pianeta_r);
+                }
+                case 4 -> {
+                    p1.stampa_lune();
+                    String nome_r = InputData.readNonEmptyString("Inserisci il nome della luna da rimuovere: ", true);
+                    Luna luna_r = (Luna) p1.toCorpo(nome_r);
+                    p1.rimuovi_luna(luna_r);
+                }
+                default -> {
+                }
             }
-            case 4 -> {
-                p1.stampa_lune();
-                String nome_r = InputData.readNonEmptyString("Inserisci il nome della luna da rimuovere: ", true);
-                Luna luna_r = (Luna) p1.toCorpo(nome_r);
-                p1.rimuovi_luna(luna_r);
-            }
-        }
+            principale = InputData.readYesOrNo("Vuoi tornare al menu principale?");
+            if(principale)
+                interazione(p1);
+            else editor(p1);
     }
 
     public void info(Planetario p1) {
         int scelta = m2.choose();
+        boolean principale;
         switch (scelta) {
             case 1 -> p1.stampa_lista_corpi();
             case 2 -> p1.stampa_info_corpo();
@@ -50,15 +58,20 @@ public class MenuPrincipale {
                 String nome_corpo = InputData.readNonEmptyString("Quale corpo vuoi controllare che sia nel sistema?\n>", true);
                 p1.appartiene(nome_corpo);
             }
+            default -> {}
         }
+        principale = InputData.readYesOrNo("Vuoi tornare al menu principale?");
+        if(principale)
+            interazione(p1);
+        else info(p1);
     }
 
     public void funzionalita(Planetario p1) {
         int scelta = m3.choose();
+        boolean principale;
         switch (scelta) {
             case 1 ->
-                    System.out.println("Il centro di massa del sistema si trova nel punto C = (" + p1.CDM().getX() + " , " + p1.CDM().getY() + ")\n");
-
+                System.out.println("Il centro di massa del sistema si trova nel punto C = (" + String.format("%.3f", p1.CDM().getX())  + " , " + String.format("%.3f", p1.CDM().getY()) + ")\n");
             case 2 -> {
                 //con un doppio ciclo for controlla che ciascun corpo possa collidere con un altro e stampa un avviso in caso affermativo
                 Corpo c1, c2;
@@ -70,17 +83,21 @@ public class MenuPrincipale {
                             System.out.println(AnsiColors.RED + "ATTENZIONE\ti corpi " + c1.getNome() + " e " + c2.getNome() + " potrebbero COLLIDERE in futuro\t\t" + AnsiColors.WHITE + "è consigliabile prendere dei provvedimenti a riguardo\n" + AnsiColors.RESET);
                     }
                 }
-
-
             }
             case 3 -> p1.calcolo_rotta();
             case 4 -> p1.rimuovi_stella();
+            default ->{}
         }
+        if (p1.getLista_corpi().size() == 0)
+            return;
+
+        principale = InputData.readYesOrNo("Vuoi tornare al menu principale?");
+        if(principale)
+            interazione(p1);
+        else funzionalita(p1);
     }
 
     public void interazione(Planetario p1) {
-        boolean continuare;
-        do {
             int scelt = menu.choose();
             switch (scelt) {
                 case 1 -> editor(p1);
@@ -89,12 +106,6 @@ public class MenuPrincipale {
                 default -> {
                 }
             }
-            if (p1.getLista_corpi().size() == 0) {
-                break;
-            }
-
-            continuare = InputData.readYesOrNo("Vuoi fare altro?");
-        } while (continuare);
     }
 
 
